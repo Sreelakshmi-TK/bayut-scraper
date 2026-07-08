e44e7155ef8b5dcf6330b5a062bcbe06978adfa6
Sreelakshmi T K <sreelakshmitk03@gmail.com>
2026-07-08 23:57:09 +0530

    Extract property listing URLs using xpath

--- bayut_scraper/spiders/bayut.py
+++ bayut_scraper/spiders/bayut.py
@@ -5,4 +5,9 @@ class BayutSpider(scrapy.Spider):
     start_urls = ["https://www.bayut.eg/en/egypt/properties-for-sale/"]
 
     def parse(self, response):
-        print(response.css("title::text").get())
+        property_cards = response.xpath("//li[@role='article']")
+        print(f"found {len(property_cards)} property cards")
+        
+        for card in property_cards:
+            link = card.xpath(".//a/@href").get()
+            print(link)
